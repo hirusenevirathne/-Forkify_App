b055d244cbb032901b1f831573a9208fe8c3b946
hirusenevirathne <107979195+hirusenevirathne@users.noreply.github.com>
2023-08-28 22:27:57 +0530

    checkpoint : need to confirm _generateMarkup running

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -14,6 +14,7 @@ class RecipeView extends View {
   }
 
   _generateMarkup() {
+    console.log('_generateMarkup Run');
     return `
   <figure class="recipe__fig">
     <img src="${this._data.image}" alt="${
